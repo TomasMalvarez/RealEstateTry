14efda548d96ca83228ed25e651f6ceae549f6f7
Necrover <paul.j.brossard@gmail.com>
2021-03-07 21:49:08 -0300

    prueba

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
+import { PropertyViewerComponent } from './components/property-viewer/property-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    PropertyViewerComponent
   ],
   imports: [
     BrowserModule,
